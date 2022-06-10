ab417d638e6195a3a0cffcfd18653f07c38711fa
gregboleet <gbolet@vt.edu>
2022-06-10 13:53:19 -0700

    updated timer to be ntp independent

--- doStaticRunsMPI_PA.sh
+++ doStaticRunsMPI_PA.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=Lyre_PA_MPI_Testing # Job name
-#SBATCH --ntasks=13                  # Number of MPI tasks (i.e. processes)
-#SBATCH --nodes=13                    # Maximum number of nodes to be allocated
+#SBATCH --ntasks=20                  # Number of MPI tasks (i.e. processes)
+#SBATCH --nodes=20                    # Maximum number of nodes to be allocated
 #SBATCH --time=05:00:00              
-#SBATCH --output=mpiRunLogs.txt     
+#SBATCH --output=mpiRunLogs_PA.txt     
 #SBATCH --open-mode=truncate
 #ignore SBATCH --partition=pdebug
 
-#NUM_NODES=4
-#srun -n${NUM_NODES} -N${NUM_NODES} --partition=pdebug --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
-#sbatch -n${NUM_NODES} -N${NUM_NODES} --time="01:00:00" --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
-#sbatch -n8 -N8 --time="03:00:00" --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
+# NUM_NODES=4
+# srun -n${NUM_NODES} -N${NUM_NODES} --partition=pdebug --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
+# sbatch -n${NUM_NODES} -N${NUM_NODES} --time="01:00:00" --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
+# sbatch -n8 -N8 --time="03:00:00" --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
 
-#srun -n8 -N8 --partition=pdebug --time="00:05:00" --open-mode=truncate --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
+# srun -n8 -N8 --partition=pdebug --time="00:05:00" --open-mode=truncate --output=mpiRunLogs.txt python3 doStaticRunsMPI.py
 srun python3 doStaticRunsMPI.py --usePA
+# srun /bin/hostname
+
+# srun -n3 -N3 myprogram &
+# srun -n3 -N3 myotherprogram &
