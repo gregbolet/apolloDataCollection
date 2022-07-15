81f0399e22d99f1bad9c4aa771bbe991eb560627
gregboleet <gbolet@vt.edu>
2022-07-14 17:01:20 -0700

    going to update script to use new_benchmarks

--- createOracleDatasets.py
+++ createOracleDatasets.py
@@ -25,52 +25,67 @@ def main():
 	                    help='Should we use PA data instead of VA?')
 	parser.add_argument('--singlemodel', action='store_true',
 	                    help='Should we make a single dataset model?')
+	parser.add_argument('-b', '--benchmarks', nargs='+', default=[], 
+											help='what benchmarks to create datasets for?')
+	parser.add_argument('-k', '--colsToKeep', nargs='+', default=[], 
+											help='What feature columns to keep when creating a dataset?')
+	parser.add_argument('--onlyAllProbs', action='store_true',
+	                    help='Should we skip making an oracle for each problem size?')
 	args = parser.parse_args()
 
-	prognames = list(progs.keys())
+	if len(args.benchmarks) == 0:
+		prognames = list(progs.keys())
+	else:
+		prognames = args.benchmarks
+
+
 	pava = 'VA'
 	if args.usePA:
 		pava = 'PA'
 
 	# Go through each program
 	for progname in prognames:
-	#for progname in ['lulesh']:
+	#for progname in ['nas_bt']:
 		prog = progs[progname]
 		exedir = rootdir+prog['exedir']
 		progsuffix = prog['suffix'][1:]
 
 		apollo_data_dir = exedir+'/'+progsuffix+'-data'
 		
-		# for each problem size, let's gather the traces to pass into the create-datasets script
-		# we're going to create an oracle for each problem size. The create-datasets script
-		# will create an optimal dataset for each region
-		for probSize in probSizes:
-			oracle_output_dir = apollo_data_dir+'/'+progsuffix+'-'+probSize+'-oracle-'+pava
-			print('writing oracle output to:', oracle_output_dir)
+		if not args.onlyAllProbs:
+			# for each problem size, let's gather the traces to pass into the create-datasets script
+			# we're going to create an oracle for each problem size. The create-datasets script
+			# will create an optimal dataset for each region
+			for probSize in probSizes:
+				oracle_output_dir = apollo_data_dir+'/'+progsuffix+'-'+probSize+'-oracle-'+pava
+				print('writing oracle output to:', oracle_output_dir)
 
-			try:
-				print('Making:', oracle_output_dir)
-				os.mkdir(oracle_output_dir)
-			except OSError as error:
-				print('oracle dir may already exist, not creating...', error)
+				try:
+					print('Making:', oracle_output_dir)
+					os.mkdir(oracle_output_dir)
+				except OSError as error:
+					print('oracle dir may already exist, not creating...', error)
 
-			# Go into the oracle folder since the create-datasets will dump here
-			os.chdir(oracle_output_dir)
+				# Go into the oracle folder since the create-datasets will dump here
+				os.chdir(oracle_output_dir)
 
-			#command = debugRun+' python3 '+create_dataset_script+' --agg mean-min --tracedirs'
-			if args.singlemodel:
-				command = 'python3 '+create_dataset_script+' --agg mean-min --singlemodel --tracedirs '
-			else:
-				command = 'python3 '+create_dataset_script+' --agg mean-min --tracedirs '
+				#command = debugRun+' python3 '+create_dataset_script+' --agg mean-min --tracedirs'
+				command = 'python3 '+create_dataset_script+' --agg mean-min '
+				if args.singlemodel:
+					command += '--singlemodel '
 
-			# gather all the trial directories and add them to the command
-			trialdirs = list(glob.glob('%s/%s-%s-trial*-%s-traces/' % (apollo_data_dir, progsuffix, probSize, pava)))
-			for trialdir in trialdirs: 
-				command += trialdir+' '
+				if len(args.colsToKeep) != 0:
+					command += '--colsToKeep '+' '.join(args.colsToKeep)+' '
 
-			print('Going to execute command:', command)
-			print()
-			os.system(command)
+				command += '--tracedirs '
+				# gather all the trial directories and add them to the command
+				trialdirs = list(glob.glob('%s/%s-%s-trial*-%s-traces/' % (apollo_data_dir, progsuffix, probSize, pava)))
+				for trialdir in trialdirs: 
+					command += trialdir+' '
+
+				print('Going to execute command:', command)
+				print()
+				os.system(command)
 
 		# now let's make one oracle for the whole program using all the data across problem sizes
 		oracle_output_dir = apollo_data_dir+'/'+progsuffix+'-allprobs-oracle-'+pava
@@ -83,10 +98,14 @@ def main():
 			print('oracle dir may already exist, not creating...', error)
 		os.chdir(oracle_output_dir)
 
+		command = 'python3 '+create_dataset_script+' --agg mean-min '
 		if args.singlemodel:
-			command = 'python3 '+create_dataset_script+' --agg mean-min --singlemodel --tracedirs '
-		else:
-			command = 'python3 '+create_dataset_script+' --agg mean-min --tracedirs '
+			command += '--singlemodel '
+
+		if len(args.colsToKeep) != 0:
+			command += '--colsToKeep '+' '.join(args.colsToKeep)+' '
+
+		command += '--tracedirs '
 		trialdirs = list(glob.glob('%s/%s-*-trial*-%s-traces/' % (apollo_data_dir, progsuffix, pava)))
 		for trialdir in trialdirs: 
 			command += trialdir+' '
